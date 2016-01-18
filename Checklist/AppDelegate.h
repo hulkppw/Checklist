155cc914b53be9b6a4e6d152715b3391602309ee
Vincen <vincen@vincendemacbook-pro.local>
2016-01-19 00:25:20 +0800

    Initial Commit

---
+++ Checklist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checklist
+//
+//  Created by Vincen on 16/1/19.
+//  Copyright © 2016年 Vincen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
